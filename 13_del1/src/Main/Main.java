f6107c11439fe622ed21b32a8bcec17787bf0365
Veli Kerim Celik <vkcelik@gmail.com>
2013-02-16 16:34:22 +0100

    removed TestData class

--- 13_del1/src/Main/Main.java
+++ 13_del1/src/Main/Main.java
@@ -3,8 +3,8 @@ package Main;
 public class Main {
 
 	public static void main(String[] args) {
-		TestData test = new TestData();
-		test.run();
+		Data d = new Data();
+		d.attemptLogin();
 	}
 	
 }
--- 13_del1/src/Main/TestData.java
+++
@@ -1,9 +0,0 @@
-package Main;
-
-public class TestData {
-	public void run(){
-		Data d = new Data();
-		d.attemptLogin();
-	}
-	
-}
