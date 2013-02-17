3e23383cded7a3f7a7acb6f905cc1014f0e50ccd
PWNSOVS <smob@postnedit.dk>
2013-02-17 16:24:07 +0100

    IFunction

--- 13_del1/src/function/Function.java
+++ 13_del1/src/function/Function.java
@@ -7,35 +7,40 @@ import data.OperatoerDTO;
 import data.IOperatoerDAO.DALException;
 import data.User;
 
-public class Function {
+public class Function implements IFunction{
 	IOperatoerDAO data;
 	public Function(IOperatoerDAO data)
 	{
 		this.data = data;
 	}
+	@Override
 	public ArrayList<OperatoerDTO> getUsers() throws DALException
 	{
 		return data.getOperatoerList();
 	}
+	@Override
 	public User getUser(int ID) throws DALException
 	{
 		return data.getOperatoer(ID);
 	}
-	
+	@Override
 	public void createUser(String name, String cpr) throws DALException
 	{
 		String password = ""; //TODO Modtag nyt password fra programmet. 
 		User newUser = new OperatoerDTO(0, name, cpr,password);
 		data.createOperatoer(newUser);
 	}
+	@Override
 	public void updateUser(User user) throws DALException
 	{
 		data.updateOperatoer(user);
 	}
+	@Override
 	public void deleteUser(User user) throws DALException
 	{
 		data.deleteOperatoer(user);
 	}
+	@Override
 	public boolean login(String password, int ID) throws DALException
 	{
 		boolean loginOk = data.attemptLogin(ID, password);
