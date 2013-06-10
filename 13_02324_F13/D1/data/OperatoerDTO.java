2be192dc3794039975810b1e62768929b1ca62ae
Martine <Martine@10.16.103.161>
2013-06-10 10:47:20 +0200

    c

--- 13_02324_F13/D1/data/OperatoerDTO.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-public class OperatoerDTO extends User {
-
-	public OperatoerDTO(int oprID, String oprNavn, String cprNr, String password)
-	{
-		super(oprID,oprNavn,cprNr,password);
-	}
-}
