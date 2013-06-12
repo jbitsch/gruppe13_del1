236adbb066eb1055ee553f850b97a05b816b32bc
Jesper <Jesper@Jesper-W520>
2013-06-12 21:56:55 +0200

    ændret i vægt simulatoren.

--- 13_02324_F13/Weight/Weight/Controller.java
+++ 13_02324_F13/Weight/Weight/Controller.java
@@ -10,6 +10,7 @@ public class Controller {
 	private static String inline="";
 	private static String IndstruktionsDisplay= "";
 	private static int portdst;
+	private boolean rm20Flag = false;
 	private WeightGUI wg;
 	private Data data;
 	private DecimalFormat brutoFormat;
@@ -41,61 +42,97 @@ public class Controller {
 		try{
 
 			while (true){
-				// Henter input fra den tilsluttede klient
-				inline = data.getInput().toUpperCase();
-
-				if (inline.startsWith("DW")){
+				try {
+					// Henter input fra den tilsluttede klient
+					inline = data.getInput().toUpperCase();
+				} catch (NullPointerException e) {
+					// Hvis forbindelsen er tabt, skal programmet lukke
+					inline = "Q";
+				}
+				if(rm20Flag)
+				{
+					data.writeTo("RM20 I\r\n");
+				}
+				else if (inline.startsWith("DW")){
 					IndstruktionsDisplay="";
 					wg.writeCommand("DW");
 					wg.setMessage(IndstruktionsDisplay);
-					data.writeTo("DW"+"\r\n");
+					data.writeTo("DW A"+"\r\n");
 				}
 				else if (inline.startsWith("D")){
 					if (inline.equals("D"))
 						IndstruktionsDisplay="";
 					else
-						IndstruktionsDisplay=(inline.substring(2,
-								inline.length()));
+						IndstruktionsDisplay=(inline.substring(2,inline.length()));
+					
 					wg.writeCommand("D");
 					wg.setMessage(IndstruktionsDisplay);
-					data.writeTo("DB"+"\r\n");
+					data.writeTo("D A"+"\r\n");
 				}
 				else if (inline.startsWith("T")){
 					tara = brutto;
 					updateGUI();
 					wg.writeCommand("T");
 					String sTara = brutoFormat.format(tara);
-					data.writeTo("T " + sTara + " kg "+"\r\n");
-
+					data.writeTo("T S " + sTara + " kg "+"\r\n");
 				}
 				else if (inline.startsWith("S")){
 					String sNetto = brutoFormat.format((brutto-tara));
 					wg.writeCommand("S");
-					data.writeTo("S " + sNetto+ " kg " +"\r\n");
+					data.writeTo("S S " + sNetto+ " kg " +"\r\n");
 				}
 				else if (inline.startsWith("B")){ 
-					String temp= inline.substring(2,inline.length());
 					try
 					{
+						String temp= inline.substring(2,inline.length());
 						brutto = Double.parseDouble(temp);
 						updateGUI();
 						wg.writeCommand("B");
 						data.writeTo("DB"+"\r\n");
-					}
-					catch(NumberFormatException e)
-					{
+					}catch (IndexOutOfBoundsException e) {
+						data.writeTo("ES" + "\r\n");
+						wg.writeCommand("Ugyldig kommando: "+inline);
+					}catch(NumberFormatException e){
 						data.writeTo("ES"+"\r\n");
+						wg.writeCommand("Ugyldig kommando: "+inline);
 					}
-
 				}
 				else if ((inline.startsWith("RM20 8"))){
-					 String[] temp;
-					 String delimiter = "\"";
-					 temp = inline.split(delimiter);
-					 data.writeTo("RM20 B\r\n");
-					 wg.setMessage(temp[1]);
-					 wg.writeCommand("Venter p� svar fra RM20 8 ordre");
-					 wg.setEdit(true, false);	 
+					String[] temp;
+					String delimiter = "\"";
+					temp = inline.split(delimiter);
+
+					if(temp.length>1)
+					{
+						data.writeTo("RM20 B\r\n");
+						wg.setMessage(temp[1]);
+						wg.writeCommand("Venter paa svar fra RM20 8 ordre");
+						wg.setEdit(true, false);
+						rm20Flag = true;
+					}
+					else
+						data.writeTo("RM20 L\r\n");
+				}
+				else if((inline.startsWith("Z")))
+				{
+					brutto = 0.00;
+					tara = 0.00;
+					wg.writeCommand("Z");
+					updateGUI();
+					data.writeTo("Z A\r\n");
+				}
+				else if((inline.startsWith("I0")))
+				{
+					data.writeTo("I0 B \"I0\"\r\n");
+					data.writeTo("I0 B \"S\"\r\n");
+					data.writeTo("I0 B \"T\"\r\n");
+					data.writeTo("I0 B \"Z\"\r\n");
+					data.writeTo("I0 B \"B\"\r\n");
+					data.writeTo("I0 B \"D\"\r\n");
+					data.writeTo("I0 B \"DW\"\r\n");
+					data.writeTo("I0 B \"RM20 8\"\r\n");
+					data.writeTo("I0 B \"Q\"\r\n");
+					wg.writeCommand("I0");
 				}
 				else if ((inline.startsWith("Q"))){
 					wg.dispose();
@@ -112,7 +149,7 @@ public class Controller {
 			}
 		}
 		catch (Exception e){
-			System.out.println("Exception: "+e.getMessage());
+			e.printStackTrace();
 		}
 	}
 
@@ -133,7 +170,7 @@ public class Controller {
 					case 'E':
 						String retur = wg.getAnswer();
 						try {
-							data.writeTo("RM20 A "+retur+"\r\n");
+							data.writeTo("RM20 A \""+retur+"\"\r\n");
 
 						} catch (IOException e1) {
 							e1.printStackTrace();
@@ -141,6 +178,7 @@ public class Controller {
 						wg.setEdit(false, true);
 						wg.setMessage("");
 						wg.writeCommand("");
+						rm20Flag = false;
 						break;
 					case 'T': 
 						setTara();
--- 13_02324_F13/Weight/Weight/WeightGUI.java
+++ 13_02324_F13/Weight/Weight/WeightGUI.java
@@ -1,8 +1,6 @@
 package Weight;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.DecimalFormat;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFormattedTextField;
@@ -12,11 +10,13 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import java.awt.Font;
-import javax.swing.JTextArea;
-
 
 public class WeightGUI extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField netto;
 	private JTextField message;
@@ -36,6 +36,7 @@ public class WeightGUI extends JFrame {
 	private JLabel lblTaraVgt;
 	
 	public WeightGUI() {
+		setResizable(false);
 		
 		lock = new Object();
 		
@@ -46,175 +47,7 @@ public class WeightGUI extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnClear = new JButton("Clear");
-		btnClear.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnClear.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				fieldReturn.setText("");
-			}
-		});
-		btnClear.setBounds(295, 120, 63, 25);
-		contentPane.add(btnClear);
-		
-		btnEnter = new JButton("Enter");
-		btnEnter.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnEnter.setEnabled(false);
-		btnEnter.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				answer = fieldReturn.getText();
-				choise = 'E';
-				ok();
-			}
-		});
-		btnEnter.setBounds(421, 120, 63, 25);
-		contentPane.add(btnEnter);
-		
-		JButton btn0 = new JButton("0");
-		btn0.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(fieldReturn.getText().length() < 8)
-					fieldReturn.setText(fieldReturn.getText()+"0");
-			}
-		});
-		btn0.setBounds(358, 120, 63, 25);
-		contentPane.add(btn0);
-		
-		JButton btn7 = new JButton("7");
-		btn7.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(fieldReturn.getText().length() < 8)
-					fieldReturn.setText(fieldReturn.getText()+"7");
-			}
-		});
-		btn7.setBounds(295, 93, 63, 25);
-		contentPane.add(btn7);
-		
-		JButton btn8 = new JButton("8");
-		btn8.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(fieldReturn.getText().length() < 8)
-					fieldReturn.setText(fieldReturn.getText()+"8");
-			}
-		});
-		btn8.setBounds(358, 93, 63, 25);
-		contentPane.add(btn8);
-		
-		JButton btn9 = new JButton("9");
-		btn9.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(fieldReturn.getText().length() < 8)
-					fieldReturn.setText(fieldReturn.getText()+"9");
-			}
-		});
-		btn9.setBounds(421, 93, 63, 25);
-		contentPane.add(btn9);
-		
-		JButton btn4 = new JButton("4");
-		btn4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(fieldReturn.getText().length() < 8)
-					fieldReturn.setText(fieldReturn.getText()+"4");
-			}
-		});
-		btn4.setBounds(295, 67, 63, 25);
-		contentPane.add(btn4);
-		
-		JButton btn5 = new JButton("5");
-		btn5.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(fieldReturn.getText().length() < 8)
-					fieldReturn.setText(fieldReturn.getText()+"5");
-			}
-		});
-		btn5.setBounds(358, 67, 63, 25);
-		contentPane.add(btn5);
-		
-		JButton btn6 = new JButton("6");
-		btn6.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(fieldReturn.getText().length() < 8)
-					fieldReturn.setText(fieldReturn.getText()+"6");
-			}
-		});
-		btn6.setBounds(421, 67, 63, 25);
-		contentPane.add(btn6);
-		
-		JButton btn1 = new JButton("1");
-		btn1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(fieldReturn.getText().length() < 8)
-					fieldReturn.setText(fieldReturn.getText()+"1");
-			}
-		});
-		btn1.setBounds(295, 42, 63, 25);
-		contentPane.add(btn1);
-		
-		JButton btn2 = new JButton("2");
-		btn2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(fieldReturn.getText().length() < 8)
-					fieldReturn.setText(fieldReturn.getText()+"2");
-			}
-		});
-		btn2.setBounds(358, 42, 63, 25);
-		contentPane.add(btn2);
-		
-		JButton btn3 = new JButton("3");
-		btn3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(fieldReturn.getText().length() < 8)
-					fieldReturn.setText(fieldReturn.getText()+"3");
-			}
-		});
-		btn3.setBounds(421, 42, 63, 25);
-		contentPane.add(btn3);
-		
-		btnYes = new JButton("Yes(Y)");
-		btnYes.setEnabled(false);
-		btnYes.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				answer = "Y";
-				choise = 'E';
-				ok();
-			}
-		});
-		btnYes.setBounds(492, 42, 93, 25);
-		contentPane.add(btnYes);
-		
-		btnNo = new JButton("No(N)");
-		btnNo.setEnabled(false);
-		btnNo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				answer = "N";
-				choise = 'E';
-				ok();
-			}
-		});
-		btnNo.setBounds(492, 67, 93, 25);
-		contentPane.add(btnNo);
-		
-		btnQuit = new JButton("Quit(Q)");
-		btnQuit.setEnabled(false);
-		btnQuit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				answer = "Q";
-				choise = 'E';
-				ok();
-			}
-		});
-		btnQuit.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnQuit.setBounds(492, 93, 93, 25);
-		contentPane.add(btnQuit);
-		
-		btnTara = new JButton("Tara");
-		btnTara.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				choise = 'T';
-				ok();
-			}
-		});
-		btnTara.setBounds(12, 91, 97, 25);
-		contentPane.add(btnTara);
+		createButtons();
 		
 		message = new JTextField();
 		message.setEditable(false);
@@ -222,6 +55,10 @@ public class WeightGUI extends JFrame {
 		message.setBounds(12, 47, 270, 30);
 		contentPane.add(message);
 		
+		lblStrengModtaget = new JLabel("Streng modtaget: ");
+		lblStrengModtaget.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblStrengModtaget.setBounds(11, 157, 473, 30);
+		contentPane.add(lblStrengModtaget);
 		
 		brutto = new JFormattedTextField();
 		brutto.setEditable(false);
@@ -232,53 +69,16 @@ public class WeightGUI extends JFrame {
 		
 		netto = new JTextField();
 		netto.setEditable(false);
-		netto.setBounds(12, 13, 270, 30);
+		netto.setBounds(58, 13, 224, 30);
 		contentPane.add(netto);
 		netto.setColumns(10);
 		netto.setText("0.000 kg");
-		
-		btnUp = new JButton("");
-		btnUp.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				choise = 'U';
-				ok();
-			}
-		});
-		btnUp.setIcon(new ImageIcon(WeightGUI.class.getResource("/javax/swing/plaf/metal/icons/sortUp.png")));
-		btnUp.setBounds(183, 109, 23, 11);
-		contentPane.add(btnUp);
-		
-		btnDown = new JButton("");
-		btnDown.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				choise = 'D';
-				ok();
-			}
-		});
-		btnDown.setIcon(new ImageIcon(WeightGUI.class.getResource("/javax/swing/plaf/metal/icons/sortDown.png")));
-		btnDown.setBounds(183, 121, 23, 11);
-		contentPane.add(btnDown);
-		
-		lblStrengModtaget = new JLabel("Streng modtaget: ");
-		lblStrengModtaget.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblStrengModtaget.setBounds(11, 157, 473, 30);
-		contentPane.add(lblStrengModtaget);
 		
 		fieldReturn = new JTextField();
 		fieldReturn.setEditable(false);
 		fieldReturn.setBounds(294, 17, 190, 22);
 		contentPane.add(fieldReturn);
 		fieldReturn.setColumns(10);
-		
-		JButton btnClickToQuit = new JButton("Click to Quit");
-		btnClickToQuit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				choise = 'Q';
-				ok();	
-			}
-		});
-		btnClickToQuit.setBounds(12, 200, 139, 25);
-		contentPane.add(btnClickToQuit);
 		
 		lblTaraVgt = new JLabel("Tara vaegt: 0.000 kg");
 		lblTaraVgt.setBounds(12, 123, 153, 16);
@@ -287,6 +87,10 @@ public class WeightGUI extends JFrame {
 		JLabel lblChangeBruto = new JLabel("Change bruto:");
 		lblChangeBruto.setBounds(183, 90, 100, 16);
 		contentPane.add(lblChangeBruto);
+		
+		JLabel lblNetto = new JLabel("Netto:");
+		lblNetto.setBounds(12, 18, 56, 16);
+		contentPane.add(lblNetto);
 	}
 	public char getInput() {
 
@@ -328,8 +132,215 @@ public class WeightGUI extends JFrame {
 		btnNo.setEnabled(one);
 		btnQuit.setEnabled(one);
 		
-		btnTara.setEnabled(two);
-		btnDown.setEnabled(two);
-		btnUp.setEnabled(two);
+//		btnTara.setEnabled(two);
+//		btnDown.setEnabled(two);
+//		btnUp.setEnabled(two);
+	}
+	private void createButtons() {
+		JButton btnClear = new JButton("Clear");
+		btnClear.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fieldReturn.setText("");
+			}
+		});
+		btnClear.setBounds(295, 120, 63, 25);
+		contentPane.add(btnClear);
+		
+		btnEnter = new JButton("Enter");
+		btnEnter.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnEnter.setEnabled(false);
+		btnEnter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				answer = fieldReturn.getText();
+				fieldReturn.setText("");
+				choise = 'E';
+				ok();
+			}
+		});
+		btnEnter.setBounds(421, 120, 63, 25);
+		contentPane.add(btnEnter);
+		
+		btnYes = new JButton("Yes(Y)");
+		btnYes.setEnabled(false);
+		btnYes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				answer = "Y";
+				choise = 'E';
+				fieldReturn.setText("");
+				ok();
+			}
+		});
+		btnYes.setBounds(492, 42, 93, 25);
+		contentPane.add(btnYes);
+		
+		btnNo = new JButton("No(N)");
+		btnNo.setEnabled(false);
+		btnNo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				answer = "N";
+				choise = 'E';
+				fieldReturn.setText("");
+				ok();
+			}
+		});
+		btnNo.setBounds(492, 67, 93, 25);
+		contentPane.add(btnNo);
+		
+		btnQuit = new JButton("Quit(Q)");
+		btnQuit.setEnabled(false);
+		btnQuit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				answer = "Q";
+				choise = 'E';
+				fieldReturn.setText("");
+				ok();
+			}
+		});
+		btnQuit.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnQuit.setBounds(492, 93, 93, 25);
+		contentPane.add(btnQuit);
+		
+		JButton btn0 = new JButton("0");
+		btn0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fieldReturn.getText().length() < 8)
+					fieldReturn.setText(fieldReturn.getText()+"0");
+			}
+		});
+		btn0.setBounds(358, 120, 63, 25);
+		contentPane.add(btn0);
+		
+		JButton btn1 = new JButton("1");
+		btn1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fieldReturn.getText().length() < 8)
+					fieldReturn.setText(fieldReturn.getText()+"1");
+			}
+		});
+		btn1.setBounds(295, 42, 63, 25);
+		contentPane.add(btn1);
+		
+		JButton btn2 = new JButton("2");
+		btn2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fieldReturn.getText().length() < 8)
+					fieldReturn.setText(fieldReturn.getText()+"2");
+			}
+		});
+		btn2.setBounds(358, 42, 63, 25);
+		contentPane.add(btn2);
+		
+		JButton btn3 = new JButton("3");
+		btn3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fieldReturn.getText().length() < 8)
+					fieldReturn.setText(fieldReturn.getText()+"3");
+			}
+		});
+		btn3.setBounds(421, 42, 63, 25);
+		contentPane.add(btn3);
+		
+		JButton btn4 = new JButton("4");
+		btn4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fieldReturn.getText().length() < 8)
+					fieldReturn.setText(fieldReturn.getText()+"4");
+			}
+		});
+		btn4.setBounds(295, 67, 63, 25);
+		contentPane.add(btn4);
+		
+		JButton btn5 = new JButton("5");
+		btn5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fieldReturn.getText().length() < 8)
+					fieldReturn.setText(fieldReturn.getText()+"5");
+			}
+		});
+		btn5.setBounds(358, 67, 63, 25);
+		contentPane.add(btn5);
+		
+		JButton btn6 = new JButton("6");
+		btn6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fieldReturn.getText().length() < 8)
+					fieldReturn.setText(fieldReturn.getText()+"6");
+			}
+		});
+		btn6.setBounds(421, 67, 63, 25);
+		contentPane.add(btn6);
+		
+		JButton btn7 = new JButton("7");
+		btn7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fieldReturn.getText().length() < 8)
+					fieldReturn.setText(fieldReturn.getText()+"7");
+			}
+		});
+		btn7.setBounds(295, 93, 63, 25);
+		contentPane.add(btn7);
+		
+		JButton btn8 = new JButton("8");
+		btn8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fieldReturn.getText().length() < 8)
+					fieldReturn.setText(fieldReturn.getText()+"8");
+			}
+		});
+		btn8.setBounds(358, 93, 63, 25);
+		contentPane.add(btn8);
+		
+		JButton btn9 = new JButton("9");
+		btn9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fieldReturn.getText().length() < 8)
+					fieldReturn.setText(fieldReturn.getText()+"9");
+			}
+		});
+		btn9.setBounds(421, 93, 63, 25);
+		contentPane.add(btn9);
+		
+		btnTara = new JButton("Tara");
+		btnTara.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				choise = 'T';
+				ok();
+			}
+		});
+		btnTara.setBounds(12, 91, 97, 25);
+		contentPane.add(btnTara);
+		
+		btnDown = new JButton("");
+		btnDown.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				choise = 'D';
+				ok();
+			}
+		});
+		btnDown.setIcon(new ImageIcon(WeightGUI.class.getResource("/javax/swing/plaf/metal/icons/sortDown.png")));
+		btnDown.setBounds(183, 121, 23, 11);
+		contentPane.add(btnDown);
+		
+		btnUp = new JButton("");
+		btnUp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				choise = 'U';
+				ok();
+			}
+		});
+		btnUp.setIcon(new ImageIcon(WeightGUI.class.getResource("/javax/swing/plaf/metal/icons/sortUp.png")));
+		btnUp.setBounds(183, 109, 23, 11);
+		contentPane.add(btnUp);
+		
+		JButton btnClickToQuit = new JButton("Click to Quit");
+		btnClickToQuit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				choise = 'Q';
+				ok();	
+			}
+		});
+		btnClickToQuit.setBounds(12, 200, 139, 25);
+		contentPane.add(btnClickToQuit);
 	}
 }
