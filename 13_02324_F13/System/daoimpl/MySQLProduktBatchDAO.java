222496a34fb98c428966d4afe3b29fc7607ed6bf
S113730 <S113730@OP990-FQ6635J.b45x.dtu.dk>
2013-06-13 14:17:43 +0200

    torsdag

--- 13_02324_F13/System/daoimpl/MySQLProduktBatchDAO.java
+++ 13_02324_F13/System/daoimpl/MySQLProduktBatchDAO.java
@@ -3,7 +3,6 @@ package daoimpl;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import connector.Connector;
@@ -50,6 +49,40 @@ public class MySQLProduktBatchDAO implements IProduktBatchDAO{
 	public ArrayList<ProduktBatchDTO> getProduktBatchList() throws DALException {
 		ArrayList<ProduktBatchDTO> list = new ArrayList<ProduktBatchDTO>();
 		ResultSet rs = Connector.doQuery("SELECT pb.*,r.*, o.* FROM recept r, produktbatch pb LEFT JOIN operatoer o ON pb.opr_id = o.opr_id WHERE r.recept_id=pb.recept_id ORDER BY pb.status ASC");
+		try
+		{
+			while (rs.next()) 
+			{
+				OperatoerDTO opr = new OperatoerDTO(rs.getInt(9),rs.getString(10),rs.getString(11),rs.getString(12),rs.getString(13),rs.getString(14));
+				ReceptDTO recept = new ReceptDTO(rs.getInt(7),rs.getString(8)); 
+				
+				Timestamp start;
+				Timestamp slut;
+				try
+				{
+					start = rs.getTimestamp(4);
+				}catch(java.sql.SQLException e)
+				{
+					start = null;
+				}
+				try
+				{
+					slut = rs.getTimestamp(5);
+				}catch(java.sql.SQLException e)
+				{
+					slut = null;
+				}
+				
+				list.add(new ProduktBatchDTO(rs.getInt(1),recept, rs.getInt(3),start, slut,opr));
+			}
+		}
+		catch (SQLException e) { throw new DALException(e); }
+		return list;
+	}
+	@Override
+	public ArrayList<ProduktBatchDTO> getProduktBatchList(String receptName) throws DALException {
+		ArrayList<ProduktBatchDTO> list = new ArrayList<ProduktBatchDTO>();
+		ResultSet rs = Connector.doQuery("SELECT pb.*,r.*, o.* FROM recept r, produktbatch pb LEFT JOIN operatoer o ON pb.opr_id = o.opr_id WHERE r.recept_id=pb.recept_id AND r.recept_navn LIKE '"+receptName +"' ORDER BY pb.status ASC");
 		try
 		{
 			while (rs.next()) 
--- 13_02324_F13/System/daointerfaces/IProduktBatchDAO.java
+++ 13_02324_F13/System/daointerfaces/IProduktBatchDAO.java
@@ -7,6 +7,7 @@ import dto.ProduktBatchDTO;
 public interface IProduktBatchDAO {
 	ProduktBatchDTO getProduktBatch(int pbId) throws DALException;
 	ArrayList<ProduktBatchDTO> getProduktBatchList() throws DALException;
+	ArrayList<ProduktBatchDTO> getProduktBatchList(String receptName) throws DALException;
 	void createProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
 	void updateProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
 }
