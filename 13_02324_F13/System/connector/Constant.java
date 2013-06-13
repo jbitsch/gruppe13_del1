76434f7af221858f3191de9fe580582a022c6556
S113730 <S113730@OP990-FQ6635J.b45x.dtu.dk>
2013-06-13 11:42:44 +0200

    sd

--- 13_02324_F13/System/connector/Constant.java
+++ 13_02324_F13/System/connector/Constant.java
@@ -6,7 +6,7 @@ package connector;
 public abstract class Constant
 {
 	public static final String
-		server					= "8.70.5.118",  // database-serveren
+		server					= "80.70.5.118",  // database-serveren
 		database				= "CDIOFinal",  //"jdbcdatabase", // navnet p� din database = dit studienummer
 		username				= "CDIOgruppe13", // dit brugernavn = dit studienummer 
 		password				= "C13DIO"; // dit password som du har valgt til din database
